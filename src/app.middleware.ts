f147daab3142ecab22d05d101fbf83e5a834d31f
Abhin <abhin1511203@gmial.com>
2026-05-14 22:19:25 +0530

    test: test husky setup with conventional commit and lint:fix

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -1,17 +1,17 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
-import { AppLogger } from './shared/common/logger/logger.service';
+import { Injectable, NestMiddleware } from '@nestjs/common'
+import { NextFunction, Request, Response } from 'express'
+import { AppLogger } from './shared/common/logger/logger.service'
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
-  constructor(private readonly logger: AppLogger) {}
+  constructor(private readonly logger: AppLogger) { }
 
   use(req: Request, res: Response, next: NextFunction) {
     const { requestId, method, originalUrl } = req;
     this.logger.info(`Incoming ${method} request on ${originalUrl}`, {
       requestId,
-    });
-    next();
+    })
+    next()
   }
 }
 
