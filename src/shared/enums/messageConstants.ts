9ca730f7064c0275b25ca603de944d681d13a164
Abhin <abhin1511203@gmial.com>
2026-08-05 14:43:16 +0530

    refactor: add commented-out import examples to message constants barrel file

--- src/shared/enums/messageConstants.ts
+++ src/shared/enums/messageConstants.ts
@@ -10,4 +10,10 @@
  * Or use the convenient barrel:
  *   import { FIXTURE_ERRORS, FIXTURE_SUCCESS } from '@/shared/enums/messages';
  */
+
+/**
+ * import { FIXTURE_ERRORS }  from '@/shared/enums/messages/errors/fixture.errors';
+ *
+ * import { MESSAGE_CONSTANTS } from '../../../../shared/enums/messageConstants';
+ */
 export { MESSAGE_CONSTANTS } from './messages/index';
